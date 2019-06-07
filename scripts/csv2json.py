9a9a23096ff17c1f524f31f39d7f225d93292e04
Lodewikus2000 <leoschreuders@hotmail.com>
2019-06-07 23:08:25 +0200

    added toggle tax button

--- scripts/csv2json.py
+++ scripts/csv2json.py
@@ -4,44 +4,51 @@
 import pandas as pd
 import csv
 import json
+import pycountry_convert as convert
+
 
 from collections import defaultdict
 
 
-INPUT_CSV = "post_tax_0_100_merged.csv"
-OUTPUT_JSON = "income_after_tax.json"
+INPUT_CSV = ["..\\data\\income_post_tax.csv", "..\\data\\gini_post_tax.csv", "..\\data\\income_pre_tax.csv"]
+OUTPUT_JSON = ["..\\data\\income_post_tax.json", "..\\data\\gini_post_tax.json", "..\\data\\income_pre_tax.json"]
 
 
 
 if __name__ == '__main__':
-    #pd.set_option("display.max_columns", 999)
-    df = pd.read_csv(INPUT_CSV, delimiter=';', header=None, names=["country","variable","percentile","year","value"], usecols=["country", "percentile", "year", "value"]) #, usecols=["COU", "Country", "Variable", "Year", "Value"])
+    for i in range(len(INPUT_CSV)):
+        pd.set_option("display.max_columns", 999)
+        df = pd.read_csv(INPUT_CSV[i], delimiter=';', header=None, names=["Country", "Variable", "Percentile", "Year", "Value"], usecols=["Country", "Percentile", "Year", "Value"]) #, usecols=["COU", "Country", "Variable", "Year", "Value"])
 
 
-    print(df)
-    # df = df[df.MEASURE == "GINI"]
-    # df = df[df.AGE == "TOT"]
+        country_map = convert.map_countries();
+        print(country_map)
+
+        def transform(country_name):
+            if country_name in country_map:
+                # print(country_map[country_name])
+                return country_map[country_name]["alpha_3"]
+            elif country_name == "USA":
+                return country_name
+            else:
+                return None
+
+        df['ISO'] = df.apply(lambda row: transform(row['Country']), axis=1)
+
+        # df = df[df.MEASURE == "GINI"]
+        # df = df[df.AGE == "TOT"]
+        # df = df.set_index("TIME")
+
+        df = df[pd.to_numeric(df["Value"], errors="coerce").notnull()]
+        df["Value"] = pd.to_numeric(df["Value"], errors="coerce")
+
+        # df = df[df["Value"].apply(lambda value: isinstance(value, float) )]
+        df = df.dropna()
 
 
-    # df = df.set_index("TIME")
-    # df = df.dropna()
+        print(df)
 
-    json_data = df.to_json(orient='records')
+        json_data = df.to_json(orient='records')
 
-    with open(OUTPUT_JSON, 'w') as outfile:
-        outfile.write(json_data)
-
-
-
-
-    # df = pd.read_csv(INPUT_CSV[1], delimiter=',', usecols=["Variable", "Measure" ,"COU","Country","Year","Value"])
-    #
-    #
-    # print(df)
-    # # df = df.set_index("TIME")
-    # # df = df.dropna()
-    #
-    # json_data = df.to_json(orient='records')
-    #
-    # with open(OUTPUT_JSON[1], 'w') as outfile:
-    #     outfile.write(json_data)
+        with open(OUTPUT_JSON[i], 'w') as outfile:
+            outfile.write(json_data)
